27e61cb3ba973035f0412a3ac07b22565f1f0c58
mbc596 <marcbotetcolomer@gmail.com>
2013-08-28 17:55:17 +0200

    A little change in name
    I will change the name in future

--- src/com/android/settings/rascarlo/SystemSettings.java
+++ src/com/android/settings/rascarlo/SystemSettings.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.mbc596;
+package com.android.settings.rascarlo;
 
 import android.os.Bundle;
 import android.preference.Preference;
